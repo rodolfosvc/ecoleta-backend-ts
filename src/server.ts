b7af2ddb1732ecf9628c1c9cc85184c19db7ac66
Rodolfo Santos <larodolfo.santos@Rodolfos-MacBook-Pro.local>
2020-06-02 20:06:42 -0300

    Creating routes file

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
-
-const users = [ 
-    'user1',
-    'user2',
-    'user3'
-];
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-    return response.json(users[id]);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-    const user = { name: data.name, email: data.email };
-    return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
