5065b6420c39170cb8a45c7be2ea771dc8981d25
Rodolfo Santos <larodolfo.santos@Rodolfos-MacBook-Pro.local>
2020-06-02 18:02:49 -0300

    Creating get user endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 
 const app = express();
+const users = [ 
+    'user1',
+    'user2',
+    'user3'
+];
 
 app.get('/users', (request, response) => {
-    return response.json([ 
-        'user1',
-        'user2',
-        'user3'
-    ]);
+    return response.json(users);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    return response.json(users[id]);
 });
 
 app.post('/users', (request, response) => {
